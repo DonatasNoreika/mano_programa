d02904420e367a3a95457448f2dadd358cf1a780
Donatas Noreika <donoras@gmail.com>
2021-02-04 20:42:25 +0200

    Pakeičiau main failą vėl

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from modules.kursas import Kursas
 import requests
 
 kursas = Kursas("Python kursas")
+kursas2= Kursas("Java kursas")
 print(kursas.pavadinimas)
 
 source = requests.get('https://www.delfi.lt/')
