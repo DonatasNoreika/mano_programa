eac420d4ed7a6bb70faf83b16a3a035c2c293811
Donatas Noreika <donoras@gmail.com>
2021-02-04 20:46:45 +0200

    Pridėjau python kurso klasę

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 
-from modules.kursas import Kursas
+from modules.python_kursas import Kursas, PythonKursas
 import requests
 
 kursas = Kursas("Python kursas")
-kursas2= Kursas("Java kursas")
+kursas2 = Kursas("Java kursas")
+kursas3 = PythonKursas("Java kursas")
 print(kursas.pavadinimas)
 
 source = requests.get('https://www.delfi.lt/')
---
+++ modules/python_kursas.py
@@ -0,0 +1,4 @@
+from modules.kursas import Kursas
+
+class PythonKursas(Kursas):
+    pass
