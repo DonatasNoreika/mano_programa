127b68dfcf179648bf869fb228fa3e69d97ca6d3
Donatas Noreika <donoras@gmail.com>
2021-02-04 20:40:10 +0200

    Pakeičiau main failą, pridėjau requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
 from modules.kursas import Kursas
+import requests
 
 kursas = Kursas("Python kursas")
-
 print(kursas.pavadinimas)
+
+source = requests.get('https://www.delfi.lt/')
+print(source.status_code)
